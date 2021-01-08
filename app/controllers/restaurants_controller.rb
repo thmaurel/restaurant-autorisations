0789c2277911495535a7e1ab8628a38b745201b1
Thibaud Maurel <maurel.thibaud@free.fr>
2021-01-08 09:57:21 +0100

    pundit policies

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,44 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update]
+  def index
+    @restaurants = policy_scope(Restaurant)
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+    authorize @restaurant
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    authorize @restaurant
+    @restaurant.user = current_user
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @restaurant.update(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+end
---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,27 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    record.user == user
+  end
+
+  def update?
+    edit?
+  end
+end
